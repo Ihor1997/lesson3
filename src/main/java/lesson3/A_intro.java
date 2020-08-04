f947282e08f0397d9d23e35bf99a654e37726c05
owner <renotir5@gmail.com>
2020-08-04 21:30:52 +0300

    Add new code

---
+++ src/main/java/lesson3/A_intro.java
@@ -0,0 +1,7 @@
+package lesson3;
+
+public class A_intro {
+    public static void main(String[] args) {
+        System.out.println("hello!!!");
+    }
+}
---
+++ src/main/java/lesson3/B_byte.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+public class B_byte {
+    public static void main(String[] args) {
+        byte a = 12;
+        System.out.println(a);
+        byte b = 'a';
+        System.out.println(b);
+
+    }
+}
